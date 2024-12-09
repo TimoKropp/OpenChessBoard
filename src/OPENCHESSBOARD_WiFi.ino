8f6d10c2e99aefda291107594d110d564c921ee9
TimoKropp <kropp.timo@gmail.com>
2024-12-09 15:29:12 +0100

    git commit name test 4

--- src/OPENCHESSBOARD_WiFi.ino
+++ src/OPENCHESSBOARD_WiFi.ino
@@ -23,7 +23,7 @@ void setup() {
 
 readSettings();
 readBoardSelection();
-//test 3
+//test 4
 if (board_startupType == "WiFi"){
    DEBUG_SERIAL.println("\nRun WiFi App...");   
    run_WiFi_app();
